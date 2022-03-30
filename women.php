b85f9f678500a4ea942ce5d0bef4496f7586fe13
Bawer farhad <81622206+bawerfarhad@users.noreply.github.com>
2022-03-30 13:36:36 +0300

    man and woman are modified

--- women.php
+++ women.php
@@ -7,143 +7,154 @@ include 'Functions/CreateDb.php';
 
 ?>
 
+<link rel="stylesheet" href="Designs/Men.css">
+   <meta charset="utf-8">
+ </head>
+ <body>
 
-    <!--end navbar-->
-    <!-- odd carousel -->
-    <img class="men" src="../Picture/wo1.jpg" style="height: 550px;">
-    <div class="main-container">
-        <button class="button">Recommended For You</button>
-    </div>
-    <!-- end odd carousel -->
-    <div class="container">
-        <h1 style="text-align:center;">Always Wellcom For Shopping With Us</h1>
-        <hr>
-    </div>
-    <!-- card for men -->
-    <div class="container">
-        <h3 style="text-align:center; font-family: 'Times New Roman', Times, serif; text-align: center; color: black;">
-            Wallet For Women</h3>
-        <br>
-        <br>
-    </div>
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <form action="manage_cart.php" method="POST">
-   <div class="card">
-                <img src="../Picture/wo2.jpg" class="img-thumbnail">
-                <p class="over">Coffee Wallet</p>
-                <p class="sel"><del>$122.00</del>&nbsp;&nbsp;&nbsp;$107.50</p>
-                <button class="btn1" type="button" href="#" name="Add_To_Cart">Add to Cart</button>
-                </div>
-                 </form>
-            </div>
-            <div class="col">
-                <img src="../Picture/wo3.jpg" class="img-thumbnail">
-                <p class="over">Bag Etsy</p>
-                <p class="sel"><del>$122.00</del>&nbsp;&nbsp;&nbsp;$107.50</p>
-                <button class="btn1" type="button" href="#">Add to Cart</button>
-            </div>
-            <div class="col">
-                <img src="../Picture/wo4.jpg" class="img-thumbnail">
-                <p class="over">Passport Wallet</p>
-                <p class="sel"><del>$122.00</del>&nbsp;&nbsp;&nbsp;$107.50</p>
-                <button class="btn1" type="button" href="#">Add to Cart</button>
-            </div>
-            <div class="col">
-                <img src="../Picture/wo5.jpg" class="img-thumbnail">
-                <p class="over">Wallet Gold</p>
-                <p class="sel"><del>$122.00</del>&nbsp;&nbsp;&nbsp;$107.50</p>
-                <button class="btn1" type="button" href="#">Add to Cart</button>
-            </div>
+<title>Men</title>
+</head>
 
-        </div>
-        <div class="row">
-            <div class="col">
-                <img src="../Picture/wo6.jpg" class="img-thumbnail">
-                <p class="over">five AERO wallets</p>
-                <p class="sel"><del>$122.00</del>&nbsp;&nbsp;&nbsp;$107.50</p>
-                <button class="btn1" type="button" href="#">Add to Cart</button>
-            </div>
-            <div class="col">
-                <img src="../Picture/wo7.jpg" class="img-thumbnail">
-                <p class="over">AERO wallets</p>
-                <p class="sel"><del>$122.00</del>&nbsp;&nbsp;&nbsp;$107.50</p>
-                <button class="btn1" type="button" href="#">Add to Cart</button>
-            </div>
-            <div class="col">
-                <img src="../Picture/wo8.jpg" class="img-thumbnail">
-                <p class="over">Munekawa Wallets</p>
-                <p class="sel"><del>$122.00</del>&nbsp;&nbsp;&nbsp;$107.50</p>
-                <button class="btn1" type="button" href="#">Add to Cart</button>
-            </div>
-            <div class="col">
-                <img src="../Picture/wo9.jpg" class="img-thumbnail">
-                <p class="over">korean wallets</p>
-                <p class="sel"><del>$122.00</del>&nbsp;&nbsp;&nbsp;$107.50</p>
-                <button class="btn1" type="button" href="#">Add to Cart</button>
-            </div>
-
-        </div>
-        <div class="row">
-            <div class="col">
-                <img src="../Picture/wo10.jpg" class="img-thumbnail">
-                <p class="over">Rugrat Wallet</p>
-                <p class="sel"><del>$122.00</del>&nbsp;&nbsp;&nbsp;$107.50</p>
-                <button class="btn1" type="button" href="#">Add to Cart</button>
-            </div>
-            <div class="col">
-                <img src="../Picture/wo11.jpg" class="img-thumbnail">
-                <p class="over">Bifold wallets</p>
-                <p class="sel"><del>$122.00</del>&nbsp;&nbsp;&nbsp;$107.50</p>
-                <button class="btn1" type="button" href="#">Add to Cart</button>
-            </div>
-            <div class="col">
-                <img src="../Picture/wo12.jpg" class="img-thumbnail">
-                <p class="over">Fashion wallets</p>
-                <p class="sel"><del>$122.00</del>&nbsp;&nbsp;&nbsp;$107.50</p>
-                <button class="btn1" type="button" href="#">Add to Cart</button>
-            </div>
-            <div class="col">
-                <img src="../Picture/wo13.jpg" class="img-thumbnail">
-                <p class="over">Shangzixi Wallets</p>
-                <p class="sel"><del>$122.00</del>&nbsp;&nbsp;&nbsp;$107.50</p>
-                <button class="btn1" type="button" href="#">Add to Cart</button>
-            </div>
-
-        </div>
-        <div class="row">
-            <div class="col">
-                <img src="../Picture/wo14.jpg" class="img-thumbnail">
-                <p class="over">Brown wallet</p>
-                <p class="sel"><del>$122.00</del>&nbsp;&nbsp;&nbsp;$107.50</p>
-                <button class="btn1" type="button" href="#">Add to Cart</button>
-            </div>
-            <div class="col">
-                <img src="../Picture/wo15.jpg" class="img-thumbnail">
-                <p class="over">Thin Wallets</p>
-                <p class="sel"><del>$122.00</del>&nbsp;&nbsp;&nbsp;$107.50</p>
-                <button class="btn1" type="button" href="#">Add to Cart</button>
-            </div>
-            <div class="col">
-                <img src="../Picture/wo16.jpg" class="img-thumbnail">
-                <p class="over">Esty Wallets</p>
-                <p class="sel"><del>$122.00</del>&nbsp;&nbsp;&nbsp;$107.50</p>
-                <button class="btn1" type="button" href="#">Add to Cart</button>
-            </div>
-            <div class="col">
-                <img src="../Picture/wo17.jpg" class="img-thumbnail">
-                <p class="over">Tura Wallet</p>
-                <p class="sel"><del>$122.00</del>&nbsp;&nbsp;&nbsp;$107.50</p>
-                <button class="btn1" type="button" href="#">Add to Cart</button>
-            </div>
-
-        </div>
-    </div>
-    <!--End card for men -->
+<body>
+<!-- odd carousel -->
+<img class="men" src="Images/wo1.jpg" style="height: 550px;">
+<div class="main-container">
+    <button class="button">Recommended For You</button>
+</div>
+<!-- end odd carousel -->
+<div class="container">
+    <h1 style="text-align:center;">Always Wellcom For Shopping With Us</h1>
+    <hr>
+</div>
+<!-- card for men -->
+<div class="container">
+    <h3 style="text-align:center; font-family: 'Times New Roman', Times, serif; text-align: center; color: black;">
+        Wallet For Women</h3>
     <br>
     <br>
-    <br>
-    <br>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <form action="manage_cart.php" method="POST">
+<div class="col">
+            <img src="Images/wo2.jpg" class="img-thumbnail">
+            <p class="over">Coffee Wallet</p>
+            <p class="sel"><del>$122.00</del>&nbsp;&nbsp;&nbsp;$107.50</p>
+            <button class="btn1" type="button" href="#" name="Add_To_Cart">Add to Cart</button>
+            </div>
+             </form>
+        </div>
+        <div class="col">
+            <img src="Images/wo3.jpg" class="img-thumbnail">
+            <p class="over">Bag Etsy</p>
+            <p class="sel"><del>$122.00</del>&nbsp;&nbsp;&nbsp;$107.50</p>
+            <button class="btn1" type="button" href="#">Add to Cart</button>
+        </div>
+        <div class="col">
+            <img src="Images/wo4.jpg" class="img-thumbnail">
+            <p class="over">Passport Wallet</p>
+            <p class="sel"><del>$122.00</del>&nbsp;&nbsp;&nbsp;$107.50</p>
+            <button class="btn1" type="button" href="#">Add to Cart</button>
+        </div>
+        <div class="col">
+            <img src="Images/wo5.jpg" class="img-thumbnail">
+            <p class="over">Wallet Gold</p>
+            <p class="sel"><del>$122.00</del>&nbsp;&nbsp;&nbsp;$107.50</p>
+            <button class="btn1" type="button" href="#">Add to Cart</button>
+        </div>
+
+    </div>
+    <div class="row">
+        <div class="col">
+            <img src="Images/wo6.jpg" class="img-thumbnail">
+            <p class="over">five AERO wallets</p>
+            <p class="sel"><del>$122.00</del>&nbsp;&nbsp;&nbsp;$107.50</p>
+            <button class="btn1" type="button" href="#">Add to Cart</button>
+        </div>
+        <div class="col">
+            <img src="Images/wo7.jpg" class="img-thumbnail">
+            <p class="over">AERO wallets</p>
+            <p class="sel"><del>$122.00</del>&nbsp;&nbsp;&nbsp;$107.50</p>
+            <button class="btn1" type="button" href="#">Add to Cart</button>
+        </div>
+        <div class="col">
+            <img src="Images/wo8.jpg" class="img-thumbnail">
+            <p class="over">Munekawa Wallets</p>
+            <p class="sel"><del>$122.00</del>&nbsp;&nbsp;&nbsp;$107.50</p>
+            <button class="btn1" type="button" href="#">Add to Cart</button>
+        </div>
+        <div class="col">
+            <img src="Images/wo9.jpg" class="img-thumbnail">
+            <p class="over">korean wallets</p>
+            <p class="sel"><del>$122.00</del>&nbsp;&nbsp;&nbsp;$107.50</p>
+            <button class="btn1" type="button" href="#">Add to Cart</button>
+        </div>
+
+    </div>
+    <div class="row">
+        <div class="col">
+            <img src="Images/wo10.jpg" class="img-thumbnail">
+            <p class="over">Rugrat Wallet</p>
+            <p class="sel"><del>$122.00</del>&nbsp;&nbsp;&nbsp;$107.50</p>
+            <button class="btn1" type="button" href="#">Add to Cart</button>
+        </div>
+        <div class="col">
+            <img src="Images/wo11.jpg" class="img-thumbnail">
+            <p class="over">Bifold wallets</p>
+            <p class="sel"><del>$122.00</del>&nbsp;&nbsp;&nbsp;$107.50</p>
+            <button class="btn1" type="button" href="#">Add to Cart</button>
+        </div>
+        <div class="col">
+            <img src="Images/wo12.jpg" class="img-thumbnail">
+            <p class="over">Fashion wallets</p>
+            <p class="sel"><del>$122.00</del>&nbsp;&nbsp;&nbsp;$107.50</p>
+            <button class="btn1" type="button" href="#">Add to Cart</button>
+        </div>
+        <div class="col">
+            <img src="Images/wo13.jpg" class="img-thumbnail">
+            <p class="over">Shangzixi Wallets</p>
+            <p class="sel"><del>$122.00</del>&nbsp;&nbsp;&nbsp;$107.50</p>
+            <button class="btn1" type="button" href="#">Add to Cart</button>
+        </div>
+
+    </div>
+    <div class="row">
+        <div class="col">
+            <img src="Images/wo14.jpg" class="img-thumbnail">
+            <p class="over">Brown wallet</p>
+            <p class="sel"><del>$122.00</del>&nbsp;&nbsp;&nbsp;$107.50</p>
+            <button class="btn1" type="button" href="#">Add to Cart</button>
+        </div>
+        <div class="col">
+            <img src="Images/wo15.jpg" class="img-thumbnail">
+            <p class="over">Thin Wallets</p>
+            <p class="sel"><del>$122.00</del>&nbsp;&nbsp;&nbsp;$107.50</p>
+            <button class="btn1" type="button" href="#">Add to Cart</button>
+        </div>
+        <div class="col">
+            <img src="Images/wo16.jpg" class="img-thumbnail">
+            <p class="over">Esty Wallets</p>
+            <p class="sel"><del>$122.00</del>&nbsp;&nbsp;&nbsp;$107.50</p>
+            <button class="btn1" type="button" href="#">Add to Cart</button>
+        </div>
+        <div class="col">
+            <img src="Images/wo17.jpg" class="img-thumbnail">
+            <p class="over">Tura Wallet</p>
+            <p class="sel"><del>$122.00</del>&nbsp;&nbsp;&nbsp;$107.50</p>
+            <button class="btn1" type="button" href="#">Add to Cart</button>
+        </div>
+
+    </div>
+</div>
+<!--End card for men -->
+<br>
+<br>
+<br>
+<br>
+
+
+
+
 
 <?php include 'Footer.php';?>
